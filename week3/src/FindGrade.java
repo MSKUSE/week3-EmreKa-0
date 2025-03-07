cebd6400671f399af8ba4b45b1ff28d4b5c48c4f
EmreKa-0 <emrek@posta.mu.edu.tr>
2025-03-07 11:57:59 +0300

    week3

--- week3/src/FindGrade.java
+++ week3/src/FindGrade.java
@@ -1,22 +1,35 @@
-public class FindGrade{
+public class FindGrade {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
 
         //Get the score from the user as a file parameter
-        //Calculate the grade based on the score
-        //Print the grade to the console
-        // 90 - 100 -> A
-        // 80 - 89 -> B
-        // 70 - 79 -> C
-        // 60 - 69 -> D
-        // 0 - 59 -> F
-
-
-
+        if (args.length != 0) {
+            int score = Integer.parseInt(args[0]);
+            char grade;
+            //Calculate the grade based on the score
+            if (score < 60) {
+                grade = 'F';
+            } else if (score < 70) {
+                grade = 'D';
+            } else if (score < 80) {
+                grade = 'C';
+            } else if (score < 90) {
+                grade = 'B';
+            } else {
+                grade = 'A';
+                //Print the grade to the console
+                System.out.println(grade);
+                // 90 - 100 -> A
+                // 80 - 89 -> B
+                // 70 - 79 -> C
+                // 60 - 69 -> D
+                // 0 - 59 -> F
+            }
+        } else {
+            System.out.println("Give parameter to args");
+        }
 
 
     }
-
-
-
 }
--- week3/src/FindMax.java
+++ week3/src/FindMax.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
-
 public class FindMax {
+
+    public static void main(String[] args) {
+        int max = 0;
+        for(int i = 0;i<args.length;i++){
+            if (Integer.parseInt(args[i]) > max) {
+                max = Integer.parseInt(args[i]);
+            }
+        }
+        System.out.println(max);
+    }
 }
